6504f8ba936cdc635fb5fa2e11b37af009aa045c
justnoxx <shamatrin@reg.ru>
2014-12-23 10:32:06 +0200

    updated pod

--- lib/AnyEvent/MySQL/ConnPool.pm
+++ lib/AnyEvent/MySQL/ConnPool.pm
@@ -4,7 +4,55 @@ AnyEvent::MySQL::ConnPool
 
 =head1 DESCRIPTION
 
-Adds additional method "pool_connect" to AnyEvent::MySQL package.
+Adds additional method "pool_connect" to L<AnyEvent::MySQL> package.
+
+=head1 SYNOPSIS
+
+Similar to AnyEvent::MySQL->connect();
+
+    use AnyEvent;
+    use AnyEvent::MySQL;
+    use AnyEvent::MySQL::ConnPool;
+
+    my $connpool = AnyEvent::MySQL->connect_pool(
+        "DBI:mysql:database=test;host=127.0.0.1;port=3306",
+        "ptest",
+        "pass", {
+            PrintError      =>  1,
+            PoolSize        =>  10,
+            CheckInterval   =>  5,
+        }, 
+        sub {
+            my($dbh) = @_;
+            if( $dbh ) {
+                warn "Connect success!";
+                $dbh->pre_do("set names latin1");
+                $dbh->pre_do("set names utf8");
+            }
+            else {
+                warn "Connect fail: $AnyEvent::MySQL::errstr ($AnyEvent::MySQL::err)";
+                $end->send;
+            }
+        }
+    );
+
+=head1 METHODS
+
+=over
+
+=item B<connect_pool>
+
+Returns connected L<AnyEvent::ConnPool> object. 
+All options for connect_pool are similar to the AnyEvent::MySQL->connect method.
+But pool accepts additional options in parameters hashref(4th parameter).
+
+    AnyEvent::MySQL->connect_pool($dsn, $user, $password, {PoolSize => 5, CheckInterval => 10}, $callback);
+
+PoolSize    =>  how many connections should be created. 5 connections by default.
+
+CheckInterval   =>  Interval for ping connections. 10 seconds by default.
+
+=back
 
 =cut
 
@@ -14,9 +62,8 @@ use warnings;
 
 use AnyEvent::MySQL;
 use AnyEvent::ConnPool;
-use Data::Dumper;
 
-our $VERSION = 0.03;
+our $VERSION = 0.04;
 
 sub import {
     *{AnyEvent::MySQL::connect_pool} = sub {
